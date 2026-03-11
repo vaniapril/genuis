00d38bde0d4172597bfc3b517b8c7ecfc3458def
Ivan Prylepski <vaniapril.dev@gmail.com>
2026-03-12 00:35:21 +0300

    [Upd]: Update text style writer with new classes

--- lib/src/core/writers/module/color_extension_writer.dart
+++ lib/src/core/writers/module/color_extension_writer.dart
@@ -193,90 +193,150 @@ class ColorExtensionWriter {
   }
 
   void writeTextStyleExtensionClass(StringBuffer buffer, Module module, Map<String, Field> fields) {
-    buffer.writeln('class $colorClassName extends TextStyle {');
-    for (final name in fields.keys) {
-      buffer.writeln('final Color _$name;');
-    }
-    buffer.writeln();
-    buffer.writeln('$colorClassName._({');
-    for (final name in fields.keys) {
-      buffer.writeln('required Color $name,');
-    }
-    buffer.writeln('required TextStyle style,');
-    buffer.writeln('}) :');
-    for (final name in fields.keys) {
-      buffer.writeln('_$name = $name,');
-    }
-    buffer.writeln('super(');
-    buffer.writeln('inherit: style.inherit,');
-    buffer.writeln('color: style.color,');
-    buffer.writeln('backgroundColor: style.backgroundColor,');
-    buffer.writeln('fontSize: style.fontSize,');
-    buffer.writeln('fontWeight: style.fontWeight,');
-    buffer.writeln('fontStyle: style.fontStyle,');
-    buffer.writeln('letterSpacing: style.letterSpacing,');
-    buffer.writeln('wordSpacing: style.wordSpacing,');
-    buffer.writeln('textBaseline: style.textBaseline,');
-    buffer.writeln('height: style.height,');
-    buffer.writeln('leadingDistribution: style.leadingDistribution,');
-    buffer.writeln('locale: style.locale,');
-    buffer.writeln('foreground: style.foreground,');
-    buffer.writeln('background: style.background,');
-    buffer.writeln('shadows: style.shadows,');
-    buffer.writeln('fontFeatures: style.fontFeatures,');
-    buffer.writeln('fontVariations: style.fontVariations,');
-    buffer.writeln('decoration: style.decoration,');
-    buffer.writeln('decorationColor: style.decorationColor,');
-    buffer.writeln('decorationStyle: style.decorationStyle,');
-    buffer.writeln('decorationThickness: style.decorationThickness,');
-    buffer.writeln('debugLabel: style.debugLabel,');
-    buffer.writeln('fontFamily: style.fontFamily,');
-    buffer.writeln('overflow: style.overflow,');
-    buffer.writeln(');');
-
-    for (final theme in module.rootClass.themes) {
-      buffer.writeln('$colorClassName.$theme(TextStyle style) : this._(');
-      for (final (name, field) in fields.iterable) {
-        buffer.writeln('$name: ${field.value(theme)},');
-      }
-      buffer.writeln('style: style,');
-      buffer.writeln(');');
-    }
-
-    buffer.writeln('');
-    buffer.writeln('$colorClassName lerp($colorClassName other, double t){');
-    buffer.writeln('if (identical(this, other)) return this;');
-    buffer.writeln('return $colorClassName._(');
-    for (final name in fields.keys) {
-      buffer.writeln('$name: Color.lerp(_$name, other._$name, t) ?? _$name,');
-    }
-    buffer.writeln('style: TextStyle.lerp(this, other, t) ?? this,');
-    buffer.writeln(');');
-    buffer.writeln('}');
-
-    buffer.writeln('@override');
-    buffer.writeln('bool operator ==(Object other) {');
-    buffer.writeln('if (identical(this, other)) {');
-    buffer.writeln('return true;');
-    buffer.writeln('}');
-    buffer.writeln('if (other.runtimeType != runtimeType) {');
-    buffer.writeln('return false;');
-    buffer.writeln('}');
-    buffer.writeln('return other is $colorClassName');
-    buffer.writeln('&& super == other');
-    for (final name in fields.keys) {
-      buffer.writeln('&& other._$name == _$name');
-    }
-    buffer.writeln(';');
-    buffer.writeln('}');
-
-    buffer.writeln();
-    for (final name in fields.keys) {
-      buffer.writeln('TextStyle get $name => copyWith(color: _$name);');
-    }
-    buffer.writeln();
-    buffer.writeln('TextStyle colored(Color? color) => copyWith(color: color);');
-
-    buffer.writeln('}');
+    ClassCode(
+      name: colorClassName,
+      extends_: 'TextStyle',
+      body: [
+        for (final name in fields.keys) FieldCode.final_('Color', '_$name'),
+        ConstructorCode(
+          type: colorClassName,
+          name: '_',
+          params: ParamsCode(
+            namedParams: [
+              for (final name in fields.keys)
+                ParamCode(
+                  required_: true,
+                  type: 'Color',
+                  name: name,
+                ),
+              ParamCode(
+                required_: true,
+                type: 'TextStyle',
+                name: 'style',
+              ),
+            ],
+          ),
+          redirect: [
+            for (final name in fields.keys) '_$name = $name,',
+            'super(',
+            'inherit: style.inherit,',
+            'color: style.color,',
+            'backgroundColor: style.backgroundColor,',
+            'fontSize: style.fontSize,',
+            'fontWeight: style.fontWeight,',
+            'fontStyle: style.fontStyle,',
+            'letterSpacing: style.letterSpacing,',
+            'wordSpacing: style.wordSpacing,',
+            'textBaseline: style.textBaseline,',
+            'height: style.height,',
+            'leadingDistribution: style.leadingDistribution,',
+            'locale: style.locale,',
+            'foreground: style.foreground,',
+            'background: style.background,',
+            'shadows: style.shadows,',
+            'fontFeatures: style.fontFeatures,',
+            'fontVariations: style.fontVariations,',
+            'decoration: style.decoration,',
+            'decorationColor: style.decorationColor,',
+            'decorationStyle: style.decorationStyle,',
+            'decorationThickness: style.decorationThickness,',
+            'debugLabel: style.debugLabel,',
+            'fontFamily: style.fontFamily,',
+            'overflow: style.overflow,',
+            ')',
+          ].code,
+        ),
+        for (final theme in module.rootClass.themes)
+          ConstructorCode(
+            type: colorClassName,
+            name: theme,
+            params: ParamsCode(
+              params: [
+                ParamCode(
+                  type: 'TextStyle',
+                  name: 'style',
+                ),
+              ],
+            ),
+            redirect: [
+              'this._(',
+              for (final (name, field) in fields.iterable) '$name: ${field.value(theme)},',
+              'style: style,',
+              ')',
+            ].code,
+          ),
+        MethodCode(
+          name: 'lerp',
+          type: colorClassName,
+          params: ParamsCode(
+            params: [
+              ParamCode(
+                type: colorClassName,
+                name: 'other',
+              ),
+              ParamCode(
+                type: 'double',
+                name: 't',
+              ),
+            ],
+          ),
+          body: [
+            'if (identical(this, other)) return this;',
+            'return $colorClassName._(',
+            for (final name in fields.keys) '$name: Color.lerp(_$name, other._$name, t) ?? _$name,',
+            'style: TextStyle.lerp(this, other, t) ?? this,',
+            ');',
+          ].code,
+        ),
+        MethodCode(
+          type: 'bool',
+          operator: true,
+          override_: true,
+          name: '==',
+          params: ParamsCode(
+            params: [
+              ParamCode(
+                type: 'Object',
+                name: 'other',
+              ),
+            ],
+          ),
+          body: [
+            'if (identical(this, other)) {',
+            'return true;',
+            '}',
+            'if (other.runtimeType != runtimeType) {',
+            'return false;',
+            '}',
+            'return other is $colorClassName',
+            '&& super == other',
+            for (final name in fields.keys) '&& other._$name == _$name',
+            ';',
+          ].code,
+        ),
+        for (final name in fields.keys)
+          MethodCode(
+            getter: true,
+            name: name,
+            type: 'TextStyle',
+            expression: true,
+            body: 'copyWith(color: _$name)'.code,
+          ),
+        MethodCode(
+          name: 'colored',
+          type: 'TextStyle',
+          expression: true,
+          params: ParamsCode(
+            params: [
+              ParamCode(
+                type: 'Color?',
+                name: 'color',
+              ),
+            ],
+          ),
+          body: 'copyWith(color: color)'.code,
+        ),
+      ],
+    ).encode(buffer);
   }
 }
