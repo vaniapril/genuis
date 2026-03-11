b78b5661a475cb820f7720a624942138476b1662
Ivan Prylepski <vaniapril.dev@gmail.com>
2026-03-12 00:57:40 +0300

    [Upd]: Update token writer with new classes

--- lib/src/core/writers/token/enum_token_writer.dart
+++ lib/src/core/writers/token/enum_token_writer.dart
@@ -1,6 +1,10 @@
 import 'package:genuis/src/core/models/code/code_entities/code_entity.dart';
+import 'package:genuis/src/core/writers/code/constructor_code.dart';
+import 'package:genuis/src/core/writers/code/enum_code.dart';
+import 'package:genuis/src/core/writers/code/field_code.dart';
+import 'package:genuis/src/core/writers/code/param_code.dart';
+import 'package:genuis/src/core/writers/code/params_code.dart';
 import 'package:genuis/src/utils/map_extension.dart';
-import 'package:genuis/src/utils/string_extension.dart';
 
 class EnumTokenWriter {
   final String className;
@@ -16,32 +20,27 @@ class EnumTokenWriter {
   void write(StringBuffer buffer, List<Field> fields) {
     if (fields.isEmpty) return;
 
-    buffer.writeln('enum $className {');
-    final StringBuffer lines = StringBuffer();
-
-    _writeFields(lines, fields);
-    final String strLines = lines.toString();
-    if (strLines.isNotEmpty) {
-      buffer.writeln(strLines.replaceLast(',', ';'));
-    }
-
-    buffer.writeln();
-    buffer.writeln('const $className(this.$valueName);');
-    buffer.writeln();
-    buffer.writeln('final $valueType $valueName;');
-    buffer.writeln('}');
-  }
-
-  void _writeFields(StringBuffer buffer, List<Field> fields) {
-    buffer.writeln();
-    for (final field in fields) {
-      _writeField(buffer, field);
-    }
-  }
-
-  void _writeField(StringBuffer buffer, Field field) {
-    for (final (theme, value) in field.values.iterable) {
-      buffer.writeln("${field.enumName(theme)}($value),");
-    }
+    EnumCode(
+      name: className,
+      items: [
+        for (final field in fields)
+          for (final (theme, value) in field.values.iterable) '${field.enumName(theme)}($value)',
+      ],
+      body: [
+        ConstructorCode(
+          const_: true,
+          type: className,
+          params: ParamsCode(
+            params: [
+              ParamCode(
+                this_: true,
+                name: valueName,
+              ),
+            ],
+          ),
+        ),
+        FieldCode.final_(valueType, valueName),
+      ],
+    ).encode(buffer);
   }
 }
